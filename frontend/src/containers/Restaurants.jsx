261711b397f842e7b3310f81c78a086b190a459d
issei-yoshi <iyoshi.engineer@gmail.com>
2023-05-06 16:49:27 +0900

    add: Restaurantsコンポーネントにてsytled-componentを用いてレイアウト調整

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,8 +1,59 @@
 import React, { useEffect } from 'react'
+import styled from 'styled-components'
 
 // API関数
 import { fetchRestaurants } from '../apis/restaurants'
 
+// Images
+import MainLogo from '../images/logo.png'
+import MainCoverImage from '../images/main-cover-image.png'
+
+// Styled-Component
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`;
+
+const MainCover = styled.img`
+  height: 600px;
+`;
+
+const RestaurantsContentsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 150px;
+`;
+
+const RestaurantsContentWrapper = styled.div`
+  width: 450px;
+  height: 300px;
+  padding: 48px;
+`;
+
+const RestaurantsImageNode = styled.img`
+  width: 100%;
+`;
+
+const MainText = styled.p`
+  color: black;
+  font-size: 18px;
+`;
+
+const SubText = styled.p`
+  color: black;
+  font-size: 12px;
+`;
+
+// ここからFunctional-Component
 export const Restaurants = () => {
 
   // 初回レンダリング時にfetchRestaurantsというAPI関数を実行するようuseEffectを使う
@@ -13,7 +64,15 @@ export const Restaurants = () => {
     )
   }, [])
 
+  // ここから描画開始
   return (
-    <div>Restaurants</div>
+    <>
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo" />
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover" />
+      </MainCoverImageWrapper>
+    </>
   )
 }
