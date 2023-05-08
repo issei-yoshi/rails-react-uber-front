6ff9f6f52efc5fdc9f67a686fdc0172db06508cd
issei-yoshi <iyoshi.engineer@gmail.com>
2023-05-08 16:50:12 +0900

    add: FoodsコンポーネントにてuseEffectを用いて初回レンダリング時にfetchFoods関数を実行するよう記載

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+
+// API関数
+import { fetchFoods } from '../apis/foods';
 
 export const Foods = () => {
   // useParamsを使うことで:以降に指定したparamsと同名の値を取得することができる
   const { restaurantId } = useParams();
+
+  // useEffectを用いて初回レンダリング時にfetchFoodsというAPI関数を実行するよう実装
+  useEffect(() => {
+    // restaurantIdを引数に取って実行する
+    fetchFoods(restaurantId)
+    // 成功時には取得したdataをコンソールに表示する
+    .then((data) =>
+      console.log(data)
+    )
+  }, [])
 
   return (
     <>
