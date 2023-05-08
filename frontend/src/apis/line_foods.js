fb8309551b6db3fb2c472023b654c792929a65d2
issei-yoshi <iyoshi.engineer@gmail.com>
2023-05-09 08:58:01 +0900

    add: line_foods/replaceにAPIを叩くreplaceLineFoods関数を定義

--- frontend/src/apis/line_foods.js
+++ frontend/src/apis/line_foods.js
@@ -1,13 +1,33 @@
 import axios from "axios";
 
 // line_foodsにAPIを叩くURLをimport
-import { lineFoods } from "../urls";
+// line_foods/replaceを叩くURLをimport
+import { lineFoods, lineFoodsReplace } from "../urls";
 
 // paramsを引数に取ってAPIを叩く関数を定義
 export const postLineFoods = (params) => {
   // URLに対してPOSTを送るためaxios.postを使用
   // 第一引数にリクエスト先のURL文字列
   return axios.post(lineFoods,
+    // 第二引数にパラメーターを渡す
+    // ここでfood_idとcountの2つをオブジェクト形式で渡すことでRailsで以下のように受け取ることが可能
+    // params[:food_id]とparams[:count]
+    {
+      food_id: params.foodId,
+      count: params.count,
+    }
+  )
+  .then(res => {
+    return res.data
+  })
+  .catch((e) => { throw e; })
+};
+
+// paramsを引数にとってAPIを叩く関数を定義
+export const replaceLineFoods = (params) => {
+  // URLに対してPUTを送るためaxios.putを使用
+  // 第一引数にリクエスト先のURL文字列
+  return axios.put(lineFoodsReplace,
     // 第二引数にパラメーターを渡す
     // ここでfood_idとcountの2つをオブジェクト形式で渡すことでRailsで以下のように受け取ることが可能
     // params[:food_id]とparams[:count]
