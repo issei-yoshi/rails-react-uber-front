bd270b29db4d4009ac1df9ccab8e14ca13048ed0
issei-yoshi <iyoshi.engineer@gmail.com>
2023-05-08 17:37:55 +0900

    add: Foodsコンポーネントにて各々で定義したstyled-componentを用いてスタイリング

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useReducer } from 'react'
+import styled from 'styled-components';
 import { useParams } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { Skeleton } from '@mui/material';
+
+// components
+import { LocalMallIcon } from '../components/Icons';
+import { FoodWrapper } from '../components/FoodWrapper';
 
 // Reducers
 import {
@@ -13,7 +20,43 @@ import { fetchFoods } from '../apis/foods';
 
 // constants(APIリクエストに関して必要な定数)
 import { REQUEST_STATE } from '../constants';
+import { COLORS } from '../style_constants';
 
+// images
+import MainLogo from '../images/logo.png';
+import FoodImage from '../images/food-image.jpg';
+
+// styled-components
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 32px;
+`;
+
+const BagIconWrapper = styled.div`
+  padding-top: 24px;
+`;
+
+const ColoredBagIcon = styled(LocalMallIcon)`
+  color: ${COLORS.MAIN};
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+
+const FoodsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  margin-bottom: 50px;
+`;
+
+const ItemWrapper = styled.div`
+  margin: 16px;
+`;
+
+// ここからFoodsコンポーネント
 export const Foods = () => {
   // useParamsを使うことで:以降に指定したparamsと同名の値を取得することができる
   const { restaurantId } = useParams();
@@ -44,20 +87,43 @@ export const Foods = () => {
 
   return (
     <>
+      <HeaderWrapper>
+        <Link to="/restaurants">
+          <MainLogoImage src={MainLogo} alt="main logo" />
+        </Link>
+        <BagIconWrapper>
+          <Link to="/orders">
+            <ColoredBagIcon fontSize="large" />
+          </Link>
+        </BagIconWrapper>
+      </HeaderWrapper>
+      <FoodsList>
       {
         // foodsStateというstateのfetchStateがLOADINGの場合という条件分岐
         foodsState.fetchState === REQUEST_STATE.LOADING ?
         <>
-          <p>ロード中</p>
+          {
+            // => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]となり、12個のSkeltonが描画される
+            [...Array(12).keys()].map(i =>
+              <ItemWrapper key={i}>
+                <Skeleton key={i} variant='rect' width={450} height={180} />
+              </ItemWrapper>
+            )
+          }
         </>
         :
         // foodsStateというstate内のfoodsListを一つ一つ取り出して表示
         foodsState.foodsList.map(food =>
-          <div key={food.id}>
-            {food.name}
-          </div>
+          <ItemWrapper key={food.id}>
+            <FoodWrapper
+              food={food}
+              onClickFoodWrapper={(food) => console.log(food)}
+              imageUrl={FoodImage}
+            />
+          </ItemWrapper>
         )
       }
+      </FoodsList>
     </>
   )
 }
