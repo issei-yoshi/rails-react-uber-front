6d636528d1c1ab9f6f6ac09768c28558136c4345
issei-yoshi <iyoshi.engineer@gmail.com>
2023-05-09 10:58:04 +0900

    add: OrdersコンポーネントにてuseEffectを用いてfetchLineFoodsというAPI関数を実行するよう実装

--- frontend/src/containers/Orders.jsx
+++ frontend/src/containers/Orders.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+// API
+import { fetchLineFoods } from '../apis/line_foods'
 
 export const Orders = () => {
+
+  // useEffectを用いて初回レンダリング時にfetchLineFoodsを実行
+  useEffect(() => {
+    fetchLineFoods()
+      .then((data) =>
+        console.log(data)
+      )
+      .catch((e) => console.error(e));
+  }, [])
+
   return (
     <div>Orders</div>
   )
